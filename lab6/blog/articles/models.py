6446531f65c9c83d39e399c41bced7849f2ec82f
dehkanovameha-art <dehkanovameha@gmail.com>
2026-03-30 12:38:00 +0300

    lab6

---
+++ lab6/blog/articles/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
